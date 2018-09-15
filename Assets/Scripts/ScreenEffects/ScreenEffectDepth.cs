1d5cbdda2bf8e102ff0de4aaefa8f9ccc286e574
Misha <mishahinkul@gmail.com>
2018-09-15 22:55:14 +0300

    [+] Add screen effect - depth

---
+++ Assets/Scripts/ScreenEffects/ScreenEffectDepth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScreenEffectDepth : MonoBehaviour {
+
+  public Shader curShader;
+
+  [Range(0, 5)]
+  public float depthPower = 1.0f;
+  private Material curMaterial;
+
+  private void Start() {
+    if (!SystemInfo.supportsImageEffects ||
+        (!curShader && !curShader.isSupported)) {
+      enabled = false;
+      return;
+    }
+    Camera.main.depthTextureMode = DepthTextureMode.Depth;
+  }
+
+  //OnRenderImage вызывается после завершения рендеринга для рендеринга изображения.
+  private void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture) {
+    if (curShader != null) {
+      CurMaterial.SetFloat("_DepthPower", depthPower);
+      Graphics.Blit(sourceTexture, destTexture, CurMaterial); //Копирует исходную текстуру в исходную текстуру с помощью шейдера.
+    }
+    else {
+      Graphics.Blit(sourceTexture, destTexture);
+    }
+  }
+
+  private void OnDisable() {
+    if(curMaterial) {
+      DestroyImmediate(curMaterial);
+    }
+  }
+
+  Material CurMaterial {
+    get {
+      if (curMaterial == null) {
+        curMaterial = new Material(curShader);
+        curMaterial.hideFlags = HideFlags.HideAndDontSave;
+      }
+
+      return curMaterial;
+    }
+  }
+}
